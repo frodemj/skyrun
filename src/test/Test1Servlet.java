e13bb23511d3b0f38c99f4603a3be1b7262ba882
Marius <toreboh@hotmail.com>
2012-11-26 07:57:34 +0100

    marius

--- src/test/Test1Servlet.java
+++ src/test/Test1Servlet.java
@@ -8,6 +8,6 @@ public class Test1Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, BlueSky ");
+		resp.getWriter().println("Hello, Marius");
 	}
 }
