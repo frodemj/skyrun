d7ca9a774730b4f26ee5238084eb0886061c0ecb
Marius Dyngeland <toreboh@hotmail.com>
2013-01-25 09:01:03 +0100

    marius servlet

---
+++ src/test/Marius.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+
+public class Marius extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		
+		
+		
+		
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, Marius");
+	}
+}
